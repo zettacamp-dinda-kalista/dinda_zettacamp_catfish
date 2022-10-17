ee9c6d25ba3db79e5aaf6a38ee14a9dc5014ce25
zettacamp-dinda-kalista <dinda.kalista@zettacamp.pro>
2022-10-17 16:00:18 +0700

    add function

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-var number1 = 9;
-var number2 = 19;
-var printResult = true;
-var resultPhrase = "Learning Typescript is different than Javascript";
-function add(n1, n2, showResult, phrase) {
-    var filter = phrase.slice(n1, n2);
-    if (showResult) {
-        return filter;
+// Case 1
+var data = [1, 'data', '3', 'result'];
+function combination(input) {
+    var commbine = input.join(" ");
+    return commbine;
+}
+var combinationInput = combination(data);
+console.log(combinationInput);
+// Case 2
+var data2 = ['Bejo', 'has', '4', 'sport', 'car'];
+function combinee(input) {
+    var result;
+    if (typeof input === 'string') {
+        result = input;
     }
     else {
-        return "Input Salah";
+        result = input.join(" ");
     }
+    return result;
 }
-var result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+var combineInput = combinee(data2);
+console.log(combineInput);
+// function combination(input: number):string{
+//     let result2;
+//     let result;
+//     // if (typeof input === 'string' || 'number') {
+//     //     result = input.join(" ");
+//     // }
+//     result2 = input;
+//     return result2;
+// }
+// let combinationInput = combination(2);
+// console.log(combinationInput);
+// console.log(typeof combinationInput);
+// const number1 = 9;
+// const number2 = 19;
+// const printResult = true;
+// const resultPhrase = "Learning Typescript is different than Javascript";
+// function add(n1: number, n2: number, showResult: boolean, phrase: string):string {
+//     const filter = phrase.slice(n1, n2);
+//     if (showResult) {
+//         return filter;
+//     } else {
+//         return "Input Salah";
+//     }
+// }
+// let result = add(number1, number2, printResult, resultPhrase);
+// console.log(result);
--- index.ts
+++ index.ts
@@ -1,15 +1,70 @@
-const number1 = 9;
-const number2 = 19;
-const printResult = true;
-const resultPhrase = "Learning Typescript is different than Javascript";
+// Case 1
+let data = [1, 'data', '3', 'result'];
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string):string {
-    const filter = phrase.slice(n1, n2);
-    if (showResult) {
-        return filter;
-    } else {
-        return "Input Salah";
+type StringOrNumber = (number | string)[];
+
+
+function combination(input: StringOrNumber):string{
+    let result;
+    if (typeof input === 'string' || 'number') {
+        result = input.join(" ");
     }
+    return result;
 }
-let result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+
+let combinationInput = combination(data);
+console.log(combinationInput);
+
+// Case 2
+
+let data2 = ['Bejo', 'has', '4', 'sport', 'car'];
+
+type Conversion = string[];
+
+function combinee(input: Conversion):string{
+    let result;
+    if (typeof input === 'string') {
+        result = input
+    } else{
+        result = input.join(" ");
+    }
+    return result;
+}
+let combineInput = combinee(data2);
+console.log(combineInput);
+
+
+
+// Type String 
+
+// let data = [1, 'data', '3', 'result'];
+
+// type StringOrNumber = (number | string)[];
+
+
+// function combination(input: StringOrNumber):string{
+//     let commbine = input.join(" ");
+//     return commbine;
+// }
+
+// let combinationInput = combination(data);
+// console.log(combinationInput);
+
+
+
+
+// Type Any
+
+// function combination(input: number):string{
+//     let result2;
+//     let result;
+//     // if (typeof input === 'string' || 'number') {
+//     //     result = input.join(" ");
+//     // }
+//     result2 = input;
+//     return result2;
+// }
+
+// let combinationInput = combination(2);
+// console.log(combinationInput);
+// console.log(typeof combinationInput);

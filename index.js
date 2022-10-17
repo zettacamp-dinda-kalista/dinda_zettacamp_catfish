c6463e0674e723cc14b7bd223f38484cdda56f9b
zettacamp-dinda-kalista <dinda.kalista@zettacamp.pro>
2022-10-17 17:00:36 +0700

    changed index

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-// Case 1
+// // Case 1
+// let data = [1, 'data', '3', 'result'];
+// type StringOrNumber = (number | string)[];
+// function combination(input: StringOrNumber):string{
+//     let result;
+//     if (typeof input === 'string' || 'number') {
+//         result = input.join(" ");
+//     }
+//     return result;
+// }
+// let combinationInput = combination(data);
+// console.log(combinationInput);
+// // Case 2
+// let data2 = ['Bejo', 'has', '4', 'sport', 'car'];
+// type Conversion = string[];
+// function combinee(input: Conversion):string{
+//     let result;
+//     if (typeof input === 'string') {
+//         result = input
+//     } else{
+//         result = input.join(" ");
+//     }
+//     return result;
+// }
+// let combineInput = combinee(data2);
+// console.log(combineInput);
+// Type String 
 var data = [1, 'data', '3', 'result'];
+var data2 = ['Bejo', 'has', '4', 'sport', 'car'];
 function combination(input) {
     var commbine = input.join(" ");
     return commbine;
 }
 var combinationInput = combination(data);
 console.log(combinationInput);
-// Case 2
-var data2 = ['Bejo', 'has', '4', 'sport', 'car'];
-function combinee(input) {
-    var result;
-    if (typeof input === 'string') {
-        result = input;
-    }
-    else {
-        result = input.join(" ");
-    }
-    return result;
-}
-var combineInput = combinee(data2);
-console.log(combineInput);
+var combinationData = combination(data2);
+console.log(combinationData);
+// Type Any
 // function combination(input: number):string{
 //     let result2;
 //     let result;
@@ -32,17 +48,3 @@ console.log(combineInput);
 // let combinationInput = combination(2);
 // console.log(combinationInput);
 // console.log(typeof combinationInput);
-// const number1 = 9;
-// const number2 = 19;
-// const printResult = true;
-// const resultPhrase = "Learning Typescript is different than Javascript";
-// function add(n1: number, n2: number, showResult: boolean, phrase: string):string {
-//     const filter = phrase.slice(n1, n2);
-//     if (showResult) {
-//         return filter;
-//     } else {
-//         return "Input Salah";
-//     }
-// }
-// let result = add(number1, number2, printResult, resultPhrase);
-// console.log(result);
--- index.ts
+++ index.ts
@@ -1,56 +1,66 @@
-// Case 1
+// Type String 
+
 let data = [1, 'data', '3', 'result'];
+let data2 = ['Bejo', 'has', '4', 'sport', 'car']
 
 type StringOrNumber = (number | string)[];
 
 
 function combination(input: StringOrNumber):string{
-    let result;
-    if (typeof input === 'string' || 'number') {
-        result = input.join(" ");
-    }
-    return result;
+    let commbine = input.join(" ");
+    return commbine;
 }
 
+//Case 1
 let combinationInput = combination(data);
 console.log(combinationInput);
 
-// Case 2
-
-let data2 = ['Bejo', 'has', '4', 'sport', 'car'];
-
-type Conversion = string[];
-
-function combinee(input: Conversion):string{
-    let result;
-    if (typeof input === 'string') {
-        result = input
-    } else{
-        result = input.join(" ");
-    }
-    return result;
-}
-let combineInput = combinee(data2);
-console.log(combineInput);
+//Case 2
+let combinationData = combination(data2);
+console.log(combinationData);
 
 
 
-// Type String 
 
+
+
+
+
+
+// // Case 1
 // let data = [1, 'data', '3', 'result'];
 
 // type StringOrNumber = (number | string)[];
 
 
 // function combination(input: StringOrNumber):string{
-//     let commbine = input.join(" ");
-//     return commbine;
+//     let result;
+//     if (typeof input === 'string' || 'number') {
+//         result = input.join(" ");
+//     }
+//     return result;
 // }
 
 // let combinationInput = combination(data);
 // console.log(combinationInput);
 
+// // Case 2
 
+// let data2 = ['Bejo', 'has', '4', 'sport', 'car'];
+
+// type Conversion = string[];
+
+// function combinee(input: Conversion):string{
+//     let result;
+//     if (typeof input === 'string') {
+//         result = input
+//     } else{
+//         result = input.join(" ");
+//     }
+//     return result;
+// }
+// let combineInput = combinee(data2);
+// console.log(combineInput);
 
 
 // Type Any
